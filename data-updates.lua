b6138553068827fb22f539cf9b2204d0697ac4cb
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-02-23 22:06:51 -0600

    Version 0.0.1

--- data-updates.lua
+++ data-updates.lua
@@ -1,11 +1,26 @@
 local reuseable = settings.startup["canisters-reuseable-canisters"].value
 
+local recycling = {}
+if mods["quality"] then recycling = require("__quality__/prototypes/recycling") end
+
 local recipe = data.raw["recipe"]["canister"]
 if recipe then
-    -- update rocket-fuel techology effects to unlock the canister recipe
+    if data.raw["item"]["tin-plate"] then
+        recipe.ingredients = { { type = "item", name = "tin-plate", amount = 1 } }
+    end
+
+    if data.raw["item"]["titanium-plate"] then
+        recipe.ingredients = { { type = "item", name = "titanium-plate", amount = 1 } }
+    end
+
+    if data.raw["item"]["glass-plate"] then
+        table.insert(recipe.ingredients, { type = "item", name = "glass-plate", amount = 1 })
+    end
+
+    if mods["quality"] then recycling.generate_recycling_recipe(recipe) end
+
     table.insert(data.raw["technology"]["rocket-fuel"].effects, { type = "unlock-recipe", recipe = "canister" })
 
-    -- conditionally set fuels to return canisters when burnt
     if reuseable then
         data.raw["item"]["rocket-fuel"].burnt_result = "canister"
         data.raw["item"]["nuclear-fuel"].burnt_result = "canister"
--- prototypes/entity/canister.lua
+++ prototypes/entity/canister.lua
@@ -10,16 +10,4 @@ item.weight = 1000
 recipe.name = name
 recipe.results = { { type = "item", name = name, amount = 1 } }
 
-if data.raw["item"]["tin-plate"] then
-    recipe.ingredients = { { type = "item", name = "tin-plate", amount = 1 } }
-end
-
-if data.raw["item"]["titanium-plate"] then
-    recipe.ingredients = { { type = "item", name = "titanium-plate", amount = 1 } }
-end
-
-if data.raw["item"]["glass-plate"] then
-    table.insert(recipe.ingredients, { type = "item", name = "glass-plate", amount = 1 })
-end
-
 data.extend({ item, recipe })
