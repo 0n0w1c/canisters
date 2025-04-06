6bcdd95c7859f332fff498ca6a0922d5cb49c853
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-04-06 11:21:10 -0500

    Version 0.2.0

--- settings.lua
+++ settings.lua
@@ -2,13 +2,14 @@ local metals = { "Steel" }
 if mods["bztin"] then table.insert(metals, "Tin") end
 if mods["bztitanium"] then table.insert(metals, "Titanium") end
 
-data:extend({
+data:extend
+({
     {
         type = "string-setting",
         name = "canisters-canister-metal",
         setting_type = "startup",
         default_value = "Steel",
         allowed_values = metals,
-        order = "b"
+        order = "a"
     },
 })
